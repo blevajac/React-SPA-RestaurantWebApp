28df9d62a2dc46d12f0225bcfeb1aa426f9422ed
bleavjac <b.levajac@gmail.com>
2017-10-02 20:29:07 +0200

    Added main marker onClick information

--- src/containers/main/contact/map.js
+++ src/containers/main/contact/map.js
@@ -2,15 +2,17 @@
 import React from 'react';
 import _ from 'lodash';
 
-import { compose, withProps, lifecycle } from "recompose";
+import { compose, withProps, lifecycle, withStateHandlers } from "recompose";
 import {
   withScriptjs,
   withGoogleMap,
   GoogleMap,
   Marker,
   DirectionsRenderer,
+  InfoWindow,
 } from "react-google-maps";
 import SearchBox from "react-google-maps/lib/components/places/StandaloneSearchBox";
+import MdRestaurantMenu from "react-icons/lib/md/restaurant-menu";
 
 const MapWithASearchBox = compose(
   withProps({
@@ -29,7 +31,7 @@ const MapWithASearchBox = compose(
            lat: 45.8131545, lng: 15.9767975
         },
         markers: [],
-        isLoggedIn: true,
+        isMainMarker: true,
         onMapMounted: ref => {
           refs.map = ref;
         },
@@ -72,7 +74,7 @@ const MapWithASearchBox = compose(
             if (status === google.maps.DirectionsStatus.OK) {
               this.setState({
                 directions: result,
-                isLoggedIn: false
+                isMainMarker: false
               });
             } else {
               console.error(`error fetching directions ${result}`);
@@ -82,6 +84,13 @@ const MapWithASearchBox = compose(
         },
       })
     },
+  }),
+  withStateHandlers(() => ({
+    isOpen: false,
+  }), {
+    onToggleOpen: ({ isOpen }) => () => ({
+      isOpen: !isOpen,
+    })
   }),
   withScriptjs,
   withGoogleMap
@@ -93,11 +102,22 @@ const MapWithASearchBox = compose(
     onBoundsChanged={props.onBoundsChanged}
   >
     {/*Lokacija marker postavljen na sam centar trga (koji nema parkioralište, a pokazivać puta gleda promet stvaraju se dupli markeri*/}
-      {props.isLoggedIn ? (
+      {props.isMainMarker ? (
         <Marker
           position={{ lat: 45.8131545, lng: 15.9767975 }}
           onClick={props.onToggleOpen}
-        />
+        >
+            {props.isOpen && <InfoWindow onCloseClick={props.onToggleOpen}>
+              <div>
+                <MdRestaurantMenu />
+                {" "}
+                Gourmet, au Gulaš
+                {" "}
+                <MdRestaurantMenu />
+                {" "}
+              </div>
+          </InfoWindow>}
+        </Marker>
       ) : (
         props.directions && <DirectionsRenderer directions={props.directions} />
       )}
@@ -105,12 +125,12 @@ const MapWithASearchBox = compose(
     <SearchBox
       ref={props.onSearchBoxMounted}
       bounds={props.bounds}
-      controlPosition={google.maps.ControlPosition.TOP_LEFT}
+      controlPosition={google.maps.ControlPosition.BOTTOM_CENTER}
       onPlacesChanged={props.onPlacesChanged}
     >
       <input
         type="text"
-        placeholder="Please, Enter your location."
+        placeholder="Please, Enter your location."        
         style={{
           boxSizing: `border-box`,
           border: `1px solid transparent`,
@@ -126,9 +146,6 @@ const MapWithASearchBox = compose(
         }}
       />
     </SearchBox>
-
-
-
   </GoogleMap>
 );
 
@@ -150,78 +167,3 @@ const Map = () => (
 );
 
 export default Map;
-
-/*
-import { compose, withProps, withStateHandlers } from "recompose";
-import MdRestaurantMenu from "react-icons/lib/md/restaurant-menu";
-import {
-  withScriptjs,
-  withGoogleMap,
-  GoogleMap,
-  Marker,
-  InfoWindow,
-} from "react-google-maps";
-
-//css
-import '../../../css/containers/main/contact/map.css';
-
-const MapWithAMakredInfoWindow = compose(
-  withProps({
-    googleMapURL: "https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places",
-    loadingElement: <div style={{ height: `750px`, width: `500px` }} />,
-    containerElement: <div style={{ height: `750px`, width: `500px` }} />,
-    mapElement: <div style={{ height: `750px`, width: `500px` }} />,
-  }),
-  withStateHandlers(() => ({
-    isOpen: false,
-  }), {
-    onToggleOpen: ({ isOpen }) => () => ({
-      isOpen: !isOpen,
-    })
-  }),
-  withScriptjs,
-  withGoogleMap
-)(props =>
-  <GoogleMap
-    defaultZoom={14}
-    defaultCenter={{ lat: 45.8131545, lng: 15.9767975 }}
-  >
-    <Marker
-      position={{ lat: 45.8131545, lng: 15.9767975 }}
-      onClick={props.onToggleOpen}
-    >
-      {props.isOpen && <InfoWindow onCloseClick={props.onToggleOpen}>
-        <div>
-          <MdRestaurantMenu />
-          {" "}
-          Gourmet, au Gulaš
-          {" "}
-          <MdRestaurantMenu />
-          {" "}
-        </div>
-
-
-      </InfoWindow>}
-    </Marker>
-  </GoogleMap>
-);
-
-const Map = () => (
-  <div className="w3-row w3-padding-64" id="about">
-    <div className="w3-col m6 w3-padding-large w3-hide-small">
-      <MapWithAMakredInfoWindow />
-    </div>
-
-    <div className="w3-col m6 w3-padding-large">
-      <h1 className="w3-center">About Catering</h1><br></br>
-
-      <h5 className="w3-center">Tradition since 1889</h5>
-      <p className="w3-large">The Catering was founded in blabla by Mr. Smith in lorem ipsum dolor sit amet, consectetur adipiscing elit consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute iruredolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.We only use <span className="w3-tag w3-light-grey">seasonal</span> ingredients.</p>
-      <p className="w3-large w3-text-grey w3-hide-medium">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod temporincididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-    </div>
-  </div>
-);
-
-export default Map;
-
-*/
