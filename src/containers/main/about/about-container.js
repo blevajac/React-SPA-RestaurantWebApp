5eb855daf60dbf26d0bfe26a3df8f574e17e3959
bleavjac <b.levajac@gmail.com>
2017-10-11 17:22:57 +0200

    made router css

--- src/containers/main/about/about-container.js
+++ src/containers/main/about/about-container.js
@@ -6,6 +6,7 @@ import {
 //css
 import '../../../index.css';
 import '../../../css/containers/main/router.css';
+import '../../../css/containers/main/about/about.css';
 
 //components
 import About from './about';
@@ -13,14 +14,14 @@ import Gallery from './gallery';
 
 const AboutContainer = () => (
   <Router>
-    <div>
+    <div className="about-border">
           <ul className="router-ul">
             <li className="router-li"><Link to="/">About</Link></li>
-            <li className="router-li"><Link to="/gallery">Gallery</Link></li>
+            <li className="router-li right-link"><Link to="/gallery">Gallery</Link></li>
           </ul>
           <Route exact path="/" component={About}/>
           <Route path="/gallery" component={Gallery}/>
-          <hr></hr>
+          
     </div>
   </Router>
 )
